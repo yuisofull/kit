a0de7fa2abedbef9a48235e7cff617d267e8c95b
yui <nhankiet0501@gmail.com>
2025-02-04 22:00:44 +0700

    integrate with sd and lb

--- examples/grpc/bidi2/cmd/client/main.go
+++ examples/grpc/bidi2/cmd/client/main.go
@@ -2,7 +2,11 @@
 package main
 
 import (
+	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/examples/grpc/bidi2/pb"
+	"github.com/go-kit/kit/sd"
+	"github.com/go-kit/kit/sd/lb"
+	"io"
 	"log"
 
 	transportgrpc "github.com/go-kit/kit/transport/grpc"
@@ -10,29 +14,47 @@ import (
 	"google.golang.org/grpc"
 )
 
-func main() {
-	addr := "localhost:8080"
-	ctx := context.Background()
-
-	conn, err := grpc.Dial(addr, grpc.WithInsecure())
-	if err != nil {
-		panic(err)
+func wrapStreamEndpoint(streamEndpoint transportgrpc.StreamingEndpoint) endpoint.Endpoint {
+	return func(context.Context, interface{}) (interface{}, error) {
+		return streamEndpoint, nil
 	}
-	defer conn.Close()
+}
 
-	client := transportgrpc.NewStreamingClient(
-		conn,
-		"/pb.MyService/StreamData",
-		func(_ context.Context, req interface{}) (interface{}, error) {
-			return req.(*pb.Message), nil
-		},
-		func(_ context.Context, resp interface{}) (interface{}, error) {
-			return resp.(*pb.Message), nil
-		},
-		pb.Message{},
-	)
+func streamEndpointFromEndpoint(e endpoint.Endpoint) transportgrpc.StreamingEndpoint {
+	se, _ := e(context.Background(), nil)
+	return se.(transportgrpc.StreamingEndpoint)
+}
 
-	streamEndpoint := client.StreamingEndpoint()
+func svcFactory() sd.Factory {
+	return func(instance string) (endpoint.Endpoint, io.Closer, error) {
+		conn, err := grpc.Dial(instance, grpc.WithInsecure())
+		if err != nil {
+			return nil, nil, err
+		}
+		client := transportgrpc.NewStreamingClient(
+			conn,
+			"/pb.MyService/StreamData",
+			func(_ context.Context, req interface{}) (interface{}, error) {
+				return req.(*pb.Message), nil
+			},
+			func(_ context.Context, resp interface{}) (interface{}, error) {
+				return resp.(*pb.Message), nil
+			},
+			pb.Message{},
+		)
+		return wrapStreamEndpoint(client.StreamingEndpoint()), conn, nil
+	}
+}
+
+func main() {
+	instancer := sd.FixedInstancer([]string{"localhost:8080"})
+	factory := svcFactory()
+	endpointer := sd.NewEndpointer(instancer, factory, nil)
+	balancer := lb.NewRoundRobin(endpointer)
+	retry := lb.Retry(3, 3, balancer)
+	streamEndpoint := streamEndpointFromEndpoint(retry)
+
+	ctx := context.Background()
 	reqCh := make(chan interface{})
 	respCh, err := streamEndpoint(ctx, reqCh)
 	if err != nil {
